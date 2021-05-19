1dc95fb5d6395df0b26cb41a503cd548b440d576
Pablo Peña <pablo@makeitworth.com.mx>
2021-05-19 13:17:59 -0500

    feat: add python-dotenv library and update settings to use env vars
    	settings.py now use dotenv library in order to keep secret
    	Django credentials

--- app/app/settings.py
+++ app/app/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9a-yq-*aq3*b0f&$s%@tl41i1l4b!%5vbf(5!0k%ry9i1o=cp('
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
